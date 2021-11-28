6941981740327e01f321d0238b1f218ffdba0de3
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2021-11-27 23:29:40 -0300

    Add file with common functions for project tests

---
+++ tests/utils.py
@@ -0,0 +1,23 @@
+import os
+import time
+import shutil
+
+
+def removeFile(filePath):
+    if os.path.exists(filePath):
+        os.remove(filePath)
+
+
+def removeDirectory(dirPath):
+    if os.path.exists(dirPath):
+        try:
+            shutil.rmtree(dirPath)
+        except WindowsError:
+            time.sleep(0.5)
+
+
+def parentDirUpdate(template, tag, target, currentDir):
+    with open(template, "r") as inputFile:
+        content = inputFile.read().replace(tag, currentDir)
+    with open(target, "w") as outputFile:
+        outputFile.write(content)
