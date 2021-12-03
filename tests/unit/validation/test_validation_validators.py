50a822438613792642b6ad1b579099bfb13f7f58
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2021-12-02 22:01:03 -0300

    Add validators unit tests

---
+++ tests/unit/validation/test_validation_validators.py
@@ -0,0 +1,23 @@
+import unittest
+
+
+class ValidatorsValidationTest(unittest.TestCase):
+    """[summary]"""
+
+    def setUp(self):
+        """Runs before each test."""
+        pass
+
+    def tearDown(self):
+        """Runs after each test."""
+        pass
+
+    def test_empty_config_list(self):
+        """"""
+        raise NotImplementedError
+
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(ValidatorsValidationTest)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
