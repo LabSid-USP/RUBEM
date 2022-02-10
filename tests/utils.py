8bf9ed9f965606a54eb33de9ff279b61e3241bbe
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2022-02-10 11:01:49 -0300

    Add function to check if two files are identical

--- tests/utils.py
+++ tests/utils.py
@@ -23,3 +23,10 @@ def parentDirUpdate(template, tags, target, dirs):
             content = content.replace(tag, dir)
     with open(target, "w") as outputFile:
         outputFile.write(content)
+
+
+def areIdendicalFiles(filePath1, filePath2):
+    if os.path.getsize(filePath1) == os.path.getsize(filePath2):
+        if open(filePath1, "r").read() == open(filePath2, "r").read():
+            return True
+    return False
