509ebb0075f59058dc2f57ec3e6b8dca6dc3c548
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2022-02-10 10:59:56 -0300

    Update function to accept lists in args

--- tests/utils.py
+++ tests/utils.py
@@ -16,8 +16,10 @@ def removeDirectory(dirPath):
             time.sleep(0.42)
 
 
-def parentDirUpdate(template, tag, target, currentDir):
+def parentDirUpdate(template, tags, target, dirs):
     with open(template, "r") as inputFile:
-        content = inputFile.read().replace(tag, currentDir)
+        content = inputFile.read()
+        for tag, dir in zip(tags, dirs):
+            content = content.replace(tag, dir)
     with open(target, "w") as outputFile:
         outputFile.write(content)
