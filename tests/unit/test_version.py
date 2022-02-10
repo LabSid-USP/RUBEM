4a0a74267b221233d9d545e1c3434bc4d3fb46eb
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2022-02-10 11:06:32 -0300

    Add version info unit tests

---
+++ tests/unit/test_version.py
@@ -0,0 +1,34 @@
+import unittest
+
+import rubem.__version__ as rbv
+
+
+class VersionTest(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_author_str(self):
+        self.assertEqual("LabSid PHA EPUSP", rbv.__author__)
+
+    def test_email_str(self):
+        self.assertEqual("rubem.hydrological@labsid.eng.br", rbv.__email__)
+
+    def test_copyright_str(self):
+        self.assertEqual(
+            "Copyright 2020-2021, LabSid PHA EPUSP", rbv.__copyright__
+        )
+
+    def test_license_str(self):
+        self.assertEqual("GPL", rbv.__license__)
+
+    def test_date_str(self):
+        self.assertEqual("2021-11-23", rbv.__date__)
+
+    def test_version_str(self):
+        self.assertEqual("0.1.0", rbv.__version__)
+
+    def test_release_str(self):
+        self.assertEqual("0.1.0-alpha", rbv.__release__)
