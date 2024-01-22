4293d1722e15a82c608d7c35ae8ac4ed6d3f7ebe
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2024-01-22 17:39:48 -0300

    Add initial unit test for `ModelConfiguration` constructor

---
+++ tests/unit/configuration/test_model_configuration.py
@@ -0,0 +1,9 @@
+import pytest
+
+from rubem.configuration.model_configuration import ModelConfiguration
+
+
+class TestModelConfiguration:
+    @pytest.mark.unit
+    def test_model_configuration_constructor(self):
+        pass
