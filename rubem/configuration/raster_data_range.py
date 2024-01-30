f16a1ff519ed85d7e6d8c6e1d1fa6bababce1d63
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2024-01-30 16:28:26 -0300

    Add `RasterDataRule` enumeration for raster data rules

---
+++ rubem/configuration/raster_data_range.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class RasterDataRule(Enum):
+    """
+    Enumeration class representing different rules for raster data.
+    """
+
+    ALLOW_ALL_ZEROES = 1
+    ALLOW_ALL_ONES = 2
+    FORBID_ALL_ZEROES = 3
+    FORBID_ALL_ONES = 4
