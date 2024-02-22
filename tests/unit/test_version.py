7f78287cdddd7796153f722ac24ba6913a51e7e8
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2024-02-22 15:21:58 -0300

    Refactor unit tests for version module

--- tests/unit/test_version.py
+++ tests/unit/test_version.py
@@ -1,35 +1,42 @@
-import unittest
+import pytest
 
-from rubem import __version__, __author__, __copyright__, \
-    __email__, __license__, __date__, __release__
+from rubem import (
+    __version__,
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __date__,
+    __release__,
+)
 
 
-class VersionTest(unittest.TestCase):
-    def setUp(self):
-        pass
+class VersionTest:
 
-    def tearDown(self):
-        pass
-
+    @pytest.mark.unit
     def test_author_str(self):
-        self.assertEqual("LabSid PHA EPUSP", __author__)
+        assert "LabSid PHA EPUSP" == __author__
 
+    @pytest.mark.unit
     def test_email_str(self):
-        self.assertEqual("rubem.hydrological@labsid.eng.br", __email__)
+        assert "rubem.hydrological@labsid.eng.br" == __email__
 
+    @pytest.mark.unit
     def test_copyright_str(self):
-        self.assertEqual(
-            "Copyright (C) 2020-2024 - LabSid/PHA/EPUSP", __copyright__
-        )
+        assert "Copyright (C) 2020-2024 - LabSid/PHA/EPUSP" == __copyright__
 
+    @pytest.mark.unit
     def test_license_str(self):
-        self.assertEqual("GPL", __license__)
+        assert "GPL" == __license__
 
+    @pytest.mark.unit
     def test_date_str(self):
-        self.assertEqual("2023-01-24", __date__)
+        assert "2023-01-24" == __date__
 
+    @pytest.mark.unit
     def test_version_str(self):
-        self.assertEqual( "0.2.3", __version__)
+        assert "0.2.3" == __version__
 
+    @pytest.mark.unit
     def test_release_str(self):
-        self.assertEqual("0.2.3-beta.2", __release__)
+        assert "0.2.3-beta.2" == __release__
