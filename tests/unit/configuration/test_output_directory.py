3831e52ebc1bf2559193da75c689e53aa695d244
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2024-02-22 09:53:55 -0300

    Refactor output directory validation and error handling

--- tests/unit/configuration/test_output_directory.py
+++ tests/unit/configuration/test_output_directory.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 
 from rubem.configuration.output_data_directory import OutputDataDirectory
@@ -6,35 +7,35 @@ from rubem.configuration.output_data_directory import OutputDataDirectory
 class TestOutputDataDirectory:
 
     @pytest.mark.unit
-    def test_create_directory_when_not_exists(self, mocker):
-        with mocker.patch("os.path.isdir", return_value=False) as mock_isdir, mocker.patch(
-            "os.makedirs"
-        ) as mock_makedirs, pytest.raises(Exception):
-            _ = OutputDataDirectory("test_path")
-            mock_isdir.assert_called_once_with("test_path")
-            mock_makedirs.assert_called_once_with("test_path")
+    def test_create_directory_when_not_exists_then_create_it(self, mocker):
+        mock_exists = mocker.patch("os.path.exists", return_value=False)
+        mock_isdir = mocker.patch("os.path.isfile", return_value=False)
+        mock_makedirs = mocker.patch("os.makedirs")
+        _ = OutputDataDirectory("test_path")
+        mock_exists.assert_called_once_with("test_path")
+        mock_isdir.assert_called_once_with("test_path")
+        mock_makedirs.assert_called_once_with("test_path")
 
     @pytest.mark.unit
     def test_directory_exists_and_empty(self, mocker):
-        with mocker.patch("os.path.isdir", return_value=True), mocker.patch(
-            "os.listdir", return_value=[]
-        ):
-            _ = OutputDataDirectory("test_path")
+        mocker.patch("os.path.exists", return_value=True)
+        mocker.patch("os.path.isfile", return_value=False)
+        mocker.patch("os.listdir", return_value=[])
+        _ = OutputDataDirectory("test_path")
 
     @pytest.mark.unit
     def test_directory_exists_and_not_empty(self, mocker):
-        with mocker.patch("os.path.isdir", return_value=True), mocker.patch(
-            "os.listdir", return_value=["file1.txt", "file2.txt", "file3.txt"]
-        ):
-            _ = OutputDataDirectory("test_path")
+        mocker.patch("os.path.exists", return_value=True)
+        mocker.patch("os.path.isfile", return_value=False)
+        mocker.patch("os.listdir", return_value=["file1.txt", "file2.txt", "file3.txt"])
+        _ = OutputDataDirectory("test_path")
 
     @pytest.mark.unit
     def test_error_creating_directory(self, mocker):
-        with mocker.patch("os.path.isdir", return_value=False), mocker.patch(
-            "os.makedirs", side_effect=Exception("error")
-        ) as mock_makedirs, mocker.patch("logging.Logger.error") as mock_error, pytest.raises(
-            Exception
-        ):
+        mocker.patch("os.path.isfile", return_value=False)
+        mock_makedirs = mocker.patch("os.makedirs", side_effect=Exception("error"))
+        mock_error = mocker.patch("logging.Logger.error")
+        with pytest.raises(Exception):
             _ = OutputDataDirectory("test_path")
             mock_makedirs.assert_called_once_with("test_path")
             mock_error.assert_called()
