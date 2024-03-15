141cab9c46cc7799f31180b6c1b60d66cfd9a8ac
Gabriel Soares <70075435+soaressgabriel@users.noreply.github.com>
2024-03-15 16:25:10 -0300

    Add alignment parameter to `SimulationPeriod` constructor

--- rubem/configuration/simulation_period.py
+++ rubem/configuration/simulation_period.py
@@ -1,5 +1,6 @@
 from datetime import date
 import logging
+from typing import Optional
 
 
 class SimulationPeriod:
@@ -12,24 +13,40 @@ class SimulationPeriod:
     :param end: The end date of the simulation period.
     :type end: date
 
+    :param alignment: The date to align the simulation period to. If not provided, the start date is used.
+    :type alignment: Optional[date]
+
     :raises ValueError: If the start date is not before the end date.
     """
 
-    def __init__(self, start: date, end: date):
+    def __init__(self, start: date, end: date, alignment: Optional[date] = None):
         self.logger = logging.getLogger(__name__)
+
         if start >= end:
-            self.logger.error("Start date must be before end date.")
-            raise ValueError("Start date must be before end date.")
+            self.logger.error("Start date (%s) must be before end date (%s).", start, end)
+            raise ValueError(f"Start date ({start}) must be before end date ({end}).")
+
         self.start_date = start
         self.end_date = end
 
-        self.total_steps = (
-            (self.end_date.year - self.start_date.year) * 12
-            + (self.end_date.month - self.start_date.month)
-            + 1
-        )
-        self.first_step = 1  # PCRaster Dynamic Framework uses 1-based indexing
-        self.last_step = self.total_steps
+        if not alignment:
+            self.logger.info("No alignment date provided. Using start date as alignment.")
+            alignment = self.start_date
+
+        if alignment < self.start_date or alignment > self.end_date:
+            self.logger.error(
+                "Date alignment must be between start (%s) and end (%s) dates.",
+                self.start_date,
+                self.end_date,
+            )
+            raise ValueError(
+                f"Date alignment must be between start ({self.start_date}) and end ({self.end_date}) dates."
+            )
+
+        self.first_step = (start.year - alignment.year) * 12 + (start.month - alignment.month) + 1
+        self.last_step = (end.year - alignment.year) * 12 + (end.month - alignment.month) + 1
+
+        self.total_steps = self.last_step - self.first_step + 1
 
     def __str__(self) -> str:
         return f"{self.start_date} to {self.end_date}"
